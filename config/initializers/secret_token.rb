944529fdfbe2476ab43565b5f57783ab2572c68e
AndySze <andy.sze.mail@gmail.com>
2012-09-30 07:35:12 +0800

    sharehub first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sharehub::Application.config.secret_token = 'aefd7c3887fc0b228a4ddd6e1a4bea983215c917b4aca79825b001858ecbb58865511a255c5a61547facbf3e7179e28e8c0f28a7d1d38f96677bc294f44f4744'
