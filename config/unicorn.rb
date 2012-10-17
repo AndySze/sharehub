7136ece5b728552c4c43f4c4b429b94a8edba2ba
AndySze <andy.sze.mail@gmail.com>
2012-10-17 21:55:07 +0800

    添加unicorn配置文件

---
+++ config/unicorn.rb
@@ -0,0 +1,9 @@
+root = "/home/andy/ShareHub/"
+working_directory root
+pid "#{root}/tmp/pids/unicorn.pid"
+stderr_path "#{root}/log/unicorn.log"
+stdout_path "#{root}/log/unicorn.log"
+
+listen "/tmp/unicorn.sharehub.sock"
+worker_processes 2
+timeout 30
