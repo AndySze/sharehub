51450323ef519f42e74670ffd534b827546592ff
AndySze <andy.sze.mail@gmail.com>
2012-11-05 14:50:41 +0800

    修改seeds文件

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,22 +7,22 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-tags = Tag.create([{title:"客厅"},
-                   {title:"卧室"},
-                   {title:"厨房"},
-                   {title:"卫生间"},
-                   {title:"餐厅"},
-                   {title:"书房"},
-                   {title:"儿童房"},
-                   {title:"玄关"},
-                   {title:"阳台"},
-                   {title:"阁楼"}])
+tags = Tag.create([{name:"客厅"},
+                   {name:"卧室"},
+                   {name:"厨房"},
+                   {name:"卫生间"},
+                   {name:"餐厅"},
+                   {name:"书房"},
+                   {name:"儿童房"},
+                   {name:"玄关"},
+                   {name:"阳台"},
+                   {name:"阁楼"}])
 
-categories = Category.create([{title:"简约风格"},
-                              {title:"田园风格"},
-                              {title:"欧式风格"},
-                              {title:"中式风格"},
-                              {title:"美式风格"},
-                              {title:"古典风格"},
-                              {title:"日式风格"}])
+categories = Category.create([{name:"简约风格"},
+                              {name:"田园风格"},
+                              {name:"欧式风格"},
+                              {name:"中式风格"},
+                              {name:"美式风格"},
+                              {name:"古典风格"},
+                              {name:"日式风格"}])
 
