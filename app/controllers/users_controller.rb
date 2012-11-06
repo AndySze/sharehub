05a65b991d1fd8a35abc10017e4ca24d2c7d6739
AndySze <andy.sze.mail@gmail.com>
2012-11-06 22:05:17 +0800

    修改样式，调整部分细节

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,6 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  def new
-    @user = User.new
-  end
-
   def index
     @users = User.all
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,11 +8,11 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
-  attr_accessible :email, :name, :user_type, :password, :password_confirmation
+  attr_accessible :email, :name, :user_type, :password,
+                  :password_confirmation, :remember_me
 
   validates :name,:presence => true,:uniqueness => true
-  validates :user_type, :presence => true
+  validates :user_type, :presence => { :message => "请选择用户类型" }
 
   has_many :designs,:through => :collections, :dependent => :destroy
   has_many :collections, :dependent => :destroy
